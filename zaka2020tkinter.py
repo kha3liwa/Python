a94cdf3c19cdc0a7dbe2ac955b0b517218d203f8
Your Name <kha3liwa@mail.net.sa>
2020-06-04 00:05:59 -0700

    upload

--- zaka2020tkinter.py
+++ zaka2020tkinter.py
@@ -4,64 +4,64 @@ win=Tk()
 win.geometry('960x500+100+100')
 win.resizable(0,0) # منع التحريك 
 win.configure(bg='#d9d9d9')
-khat=font=('arial',12,'bold')
+khat=font=('arial',22,'bold')
 khat1=font=('arial',40,'bold')
 khat2=font=('arial',90,'bold')
 var = IntVar()
 #----------------------- code ------------------------
 def zakat():
     try:
-        label_1.configure(text='الرجاء ادخال اي قيمة ذهب بالجرام:',fg='green')
+        label_1.configure(text='الرجاء ادخال اي قيمة بالجرام:',fg='green')
         lab.configure(fg='black')
         global zaka
         mony=float(en.get())
         ayar=var.get()
         zaka=float(mony*ayar/24) 
-        label_2.configure(text=zaka)   
+        label_2.configure(text=zaka)
+        canvas0.configure(bg='#d9d9d9')
     except :# Exception:
-        label_1.configure(text=" الرجاء ادخال اي قيمة ذهب بالجرام:->  ",fg='red')
+        label_1.configure(text=" الرجاء ادخال اي قيمة بالجرام:->  ",fg='red')
         lab.configure(fg='red')
+        canvas0.configure(bg='red')
 # -------------- Designer -----------------------------
-ntb = ttk.Notebook(win)
-ntb.grid()
+canvas0 = Canvas(win,bg='#AAAAFF')
+canvas0.place( x=20,y=20,width = 930, height = 460 )
 
-fm1 = Frame(ntb)
-fm2 = Frame(ntb)
-fm3 = Frame(ntb)
-ntb.add(fm1, text='frame 1')
+canvas1 = Canvas(canvas0,bg='#AAAAFF')
+canvas1.place( x=15,y=5,width = 900, height = 450 )
 
 #-------------- Combobox ------------------------------
-cb = ttk.Combobox(fm1,values=(18,21,24),textvariable=var,font=khat)
-cb.grid(row=6,column=1)
-lab=Label(fm1,text="اختر العيار للذهب")
-lab.grid(row=6,column=2)
+cb = ttk.Combobox(canvas1,font=khat,values=(18,21,24),textvariable=var)
+cb.grid(row=7,column=1)
+lab=Label(canvas1,text="اختر العيار للذهب",font=khat)
+lab.grid(row=7,column=2)
 # ------------- label Designer ------------------------
-label_1 = Label(fm1,text='الرجاء ادخال اي قيمة بالجرام:',
+label_1 = Label(canvas1,text='الرجاء ادخال اي قيمة بالجرام:',
                bg='#d9d9d9',
                fg='green',
                font=khat                
                )
-label_1.grid(row=5,column=0)
+label_1.grid(row=6,column=2)
 # ------------- Entry Designer ------------------------
-en=Entry(fm1,bg='cyan',bd=9,relief='groove',cursor='heart',
+en=Entry(canvas1,bg='cyan',bd=6,relief='groove',cursor='heart',
          font=khat,
          fg='black',
          justify='left',
-         width=19,
+         width=20,
          selectbackground='pink',
          selectforeground='red'
          )
-en.grid(row=5,column=1)
-label_2 = Label(fm1,text='<.....>',
-               bg='#d9d9d9',
+en.grid(row=6,column=1)
+label_2 = Label(canvas1,text='<.....>',
+               bg='#AAAAFF',
                fg='lime',
                font=khat2,
                state='disabled'
                )
 label_2.grid(row=8,column=1)
 
-showButton = Button(fm1, text="اظهر نصابه ",font=khat1,
+showButton = Button(canvas1, text="اظهر نصابه ",font=('arial',25,'bold'),
                     fg='grey',bg='powder blue',command=zakat)
-showButton.grid(row=7,column=0)
+showButton.grid(row=0,column=0)
 
 win.mainloop()
